6b9fdf58a7a656a369795dd14d8ea84148628be5
Aleksander Truszczyński <truszczynskia@student.mini.pw.edu.pl>
2021-01-29 17:07:16 +0100

    Done

--- lab8.m
+++ lab8.m
@@ -1,83 +1,65 @@
 function lab8()
-    Q = [1, 0, 0, 0; 0, 1, 0, 0; 0, 0, 1, 0; 0, 0, 0, 0];
-    inv(Q)
-    c = [0; 0; 0; 0];
+    H = [1, 0, 0, 0; 0, 1, 0, 0; 0, 0, 1, 0; 0, 0, 0, 0];
+    cc = [0; 0; 0; 0];
     [X, Y] = dane1();
-    
-%     Y = -Y
-    
-    size(Y,2)
-    asd = find(Y < 0)
     
     X1 = X.';
     X1(:,4) = ones(size(X1,1), 1);
     Y1 = Y.';
     
-    
     A = X1 .* Y1;
     A = -A;
     b = -ones(size(A,1),1);
-    
-    x0 = zeros(4,1);
 
     e = 1e-10;
-    ea = 1e-8;
     
     options = optimoptions('quadprog','Display','iter', 'ConstraintTolerance', e, 'OptimalityTolerance', e);
     
-    [a,fval,exitflag,output,lambda] = quadprog(Q,c,A,b,[],[],[],[], [], options);
+    [a,fval0,exitflag,output,lambda] = quadprog(H,cc,A,b,[],[],[],[], [], options);
     
-    a
-    lambda.ineqlin
+    C = diag(Y);
+    Q = C.' * (X.' * X) * C;
     
-    m = size(Y,2)
+    c = ones(size(X, 2), 1);
+    A = Y;
+    b = 0;
+    lb = zeros(size(X, 2), 1);
     
-    Q = (X.' * X ) .* (Y.' * Y) 
-    C = diag(Y)
-    Q = C.' * (X.' * X) * C
-    det(Q)
+    [x,fval,exitflag,output,llambda] = quadprog(Q, -c, [], [], A, b, lb, [], [], options);
     
-    c = ones(size(X, 2), 1)
-    A = Y
-    b = 0
-    lb = zeros(size(X, 2), 1)
+    [lllambdy, f_opt, exitflagg, it, LL] = IPM(Q, -c, A, b, e, 200, 100000)
     
-    [x,fval,exitflag,output,llambda] = quadprog(Q, -c, [], [], A, b, lb, [], [], options)
+    if exitflagg == 1
+        quadLambda = [llambda.eqlin; llambda.lower]
+        norm(LL - quadLambda)
+        norm(fval - f_opt)
+        norm(lllambdy - x)
+        norm(1/2 * a'*H*a + cc' * a + (1/2 * x' * Q * x - c' * x))
+        
+        A = X .* Y;
+        aa = A * lllambdy;
+        
+        inds = find(lllambdy > e);
+        ind = inds(1);
+        
+        A = X1 .* Y1;
+        d = A(ind, :);
     
-    norm(lambda.ineqlin - x)
-    llambda.upper
-    llambda.ineqlin
-    llambda.eqlin
-    
-%     x0 = -ones(size(A,2),1)
-    x0 = ones(size(A, 2), 1)
-    
-    
-    [lllambdy, f_opt, exitflagg, it, LL] = IPM(Q, -c, A, b, lb, x0, e, 100, 100000)
-    
-    lambda.ineqlin
-    norm(lllambdy - x)
-    lllambdy
-    norm(lambda.ineqlin - lllambdy)
-    a;
-    A = X .* Y;
-    aa = A * lllambdy;
-    
-    inds = find(lllambdy > e);
-    ind = inds(1);
-    
-    
-    
-    A = X1 .* Y1;
-    d = A(ind, :);
-    
-    L = d(4);
-    R = 1 - d(1:3) * aa(1:3);
-    
-    b = linsolve(L, R);
-    
-    a
-    aa = [aa; b]
-    
-    norm(aa - a)
+        L = d(4);
+        R = 1 - d(1:3) * aa(1:3);
+        b = linsolve(L, R);
+        aa = [aa; b];
+        
+        norm(aa - a)
+        aa - b
+        
+        wykres(X, Y, aa)
+        set(gcf,'color','w');
+        
+        
+    end
+    it
+    exitflag
+    exitflagg
+    ori = 0
 end
